1a959fca014cb23584ebb81e889cb6495d7523c3
Raúl <66863144+raulmontoro@users.noreply.github.com>
2024-06-25 13:21:46 +0200

    comments

--- src/src/PolyCube.cpp
+++ src/src/PolyCube.cpp
@@ -20,11 +20,11 @@ namespace PolyCube
         return hash;
     }
 
-    center_info compute_center(const PolyCube::PointList &coords)
-    {
+    center_info compute_center(const PolyCube::PointList &coords) {
         int16_t total_x = 0;
         int16_t total_y = 0;
         int16_t total_z = 0;
+        
         int8_t min_x = INT8_MAX;
         int8_t min_y = INT8_MAX;
         int8_t min_z = INT8_MAX;
@@ -33,25 +33,32 @@ namespace PolyCube
         int8_t max_y = INT8_MIN;
         int8_t max_z = INT8_MIN;
 
-        for (const auto &val : coords)
-        {
+        for (const auto &val : coords) {
             total_x += val.getX();
             total_y += val.getY();
             total_z += val.getZ();
+            
             min_x = std::min(min_x, val.getX());
             min_y = std::min(min_y, val.getY());
             min_z = std::min(min_z, val.getZ());
+            
             max_x = std::max(max_x, val.getX());
             max_y = std::max(max_y, val.getY());
             max_z = std::max(max_z, val.getZ());
         }
+        
+        // number of cubes in a polycubes
         size_t total = coords.size();
+        
         total_x = ::helpers::idiv_roundup(total_x, total);
         total_y = ::helpers::idiv_roundup(total_y, total);
         total_z = ::helpers::idiv_roundup(total_z, total);
+        
         Coord center(total_x, total_y, total_z);
+        
         Coord min(min_x, min_y, min_z);
         Coord max(max_x, max_y, max_z);
+        
         return center_info(center, min, max);
     }
 
